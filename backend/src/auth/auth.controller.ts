ef64d556c572afc8ef3a697442086b46f79d79db
valktaelen <alix.artiges@hotmail.fr>
2022-11-04 21:51:02 +0100

    passport + getUid

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -3,9 +3,15 @@ import { query } from 'express';
 
 @Controller('auth')
 export class AuthController {
+
     @Get('intra-connect')
     async intra_connect(@Query() infos: any, @Res() req:any) {
         console.log(infos,)
+    }
+
+    @Get('getUid')
+    getUID() {
+        return process.env.API_UID;
     }
 }
 
