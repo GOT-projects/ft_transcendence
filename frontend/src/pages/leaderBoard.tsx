44721f2a522544ff49504ae4de38aa345bcd1b06
Roymain <romain.cu69@gmail.com>
2022-11-24 06:01:37 +0100

    alix

--- frontend/src/pages/leaderBoard.tsx
+++ frontend/src/pages/leaderBoard.tsx
@@ -2,59 +2,79 @@ import BackgroundAnimate from "../components/BackGroundAnimate";
 import Footer from "../components/Footer";
 import Header from "../components/Header"
 import {Colors} from "../components/Colors"
-import React from 'react'
-import { useState } from "react";
+
+
 import { StyledLead, StyledLeadTile, StyledSep, StyledTile, StyledLeadP, StyledLeadTileRank, Button } from "../components/Styles/StyledleaderBoard";
 import {InfoServer, NotifyInter} from "../components/interfaces"
 import {Notification} from "../components/Notify"
 import { v4 as uuid } from 'uuid';
 import { apiGet } from "../api/get";
-import { GOT } from "../types";
+import { SocketContext } from "../socket/socketPovider";
+import { tmpdir } from "os";
+import React, { useContext, useState, useEffect, useRef } from 'react';
+import { GOT } from "../shared/types";
 
-function test(user: string, setTmpp: any): boolean {
-    const responce = apiGet.getHistoric(localStorage.getItem("token_access") + "", user);
-        responce.then((rep) => {
-        console.log(rep);
 
-        setTmpp(rep.data.parties);
-    }).catch(() => {setTmpp(); return false})
-    return true
+async function useInterval(callback: any, delay: number) {
+	const savedCallback: any = useRef();
+	
+	// Remember the latest callback.
+	useEffect(() => {
+		savedCallback.current = callback;
+	}, [callback]);
+	
+	// Set up the interval.
+	useEffect(() => {
+		function tick() {
+			if (savedCallback.current)
+			savedCallback.current();
+		}
+		if (delay !== null) {
+			let id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
+	}, [delay]);
 }
 
 
+
+
 const LeaderBoard = () => {
-    
-    const [tmppp, setTmpp] = useState<GOT.Party[]>();
-
     const [notify, setNotify] = useState<NotifyInter>({isOpen: false, message:'', type:''});
+    
+    const socket = useContext(SocketContext);
+    
+    const [tab, setTab] = useState<GOT.LeaderBoard>();
+    
 
-    interface Ranks{
-        id: string,
-        rank:number,
-        name:string,
-        wins:number,
-        lose:number,
-        games:string[],
-    }
-
-    const [rank] = useState<Ranks[]>([
-        {id:uuid(), rank: 1, name: "rcuminal", wins: 302, lose: 102, games: ["2-0", "5-2"]},
-        {id:uuid(), rank: 2, name: "test2", wins: 302, lose: 102, games: ["2-1", "5-2"]},
-    ]);
+    
+    socket.off("client_leaderboard");
+    
+    useEffect( () => {
+        socket.on("client_leaderboard", (e: GOT.LeaderBoard) => {
+            console.log(e);
+            //if (tab === e)
+            setTab(e);
+        })
+    }, [tab])
 
     const [clickedButton, setClickedButton] = useState('');
-
+    
 	const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
         const button: HTMLButtonElement = event.currentTarget;
-        if (test(button.name, setTmpp))
-            setClickedButton(button.name);
+        setClickedButton(button.name);
 	};
-
-// TESTER 'ALLLEADERBOARD'
+    
+    
+    
+    socket.emit("server_leaderboard", "test");
+    
+    
+    
 	return (
-
-		<React.Fragment>
+        
+        <React.Fragment>
 			<BackgroundAnimate name="LeaderBoard"/>
 			<Header colorHome={Colors.MenuDisable} 
                     colorGame={Colors.MenuDisable} 
@@ -67,87 +87,62 @@ const LeaderBoard = () => {
 				<StyledSep/>
 				<StyledLeadTileRank color={Colors.Sep}>
 				<tr>
-					<StyledLeadP>Rank</StyledLeadP>
 					<StyledLeadP>Name</StyledLeadP>
+					<StyledLeadP>Rank</StyledLeadP>
 					<StyledLeadP>Wins</StyledLeadP>
 					<StyledLeadP>Loses</StyledLeadP>
 				</tr>
-				</StyledLeadTileRank>
-				<>
-				{rank.map((rk: Ranks) => (
-					<StyledLeadTile color={Colors.Rank} key={uuid()}>
-						<tr>
-						<StyledLeadP>{rk.rank}</StyledLeadP>
-						<StyledLeadP>
-							<Button onClick={buttonHandler} className="button" name={rk.name}>{rk.name}
-							</Button>
-                                {clickedButton === rk.name 
+                <>
+
+				{
+                    tab?.map((usr: GOT.ProfileLeaderBoard) => (
+                        
+                    <tr>
+                        <Button onClick={buttonHandler} className="button" name={usr.userInfos.username}>{usr.userInfos.username}</Button>
+                        <StyledLeadP>{usr.stat.rank}</StyledLeadP>
+                        <StyledLeadP>{usr.stat.victory}</StyledLeadP>
+                        <StyledLeadP>{usr.stat.defeat}</StyledLeadP>
+                    </tr>
+                ))
+                // rank.map((rk: Ranks) => (
+                    // 	<StyledLeadTile color={Colors.Rank} key={uuid()}>
+                    // 		<tr>
+                    // 		<StyledLeadP>{rk.rank}</StyledLeadP>
+                    // 		<StyledLeadP>
+                    // 			<Button onClick={buttonHandler} className="button" name={rk.name}>{rk.name}
+                    // 			</Button>
+                //                 {clickedButton === rk.name 
                                 
-                                    ? <tr >{ tmppp?.map((game: GOT.Party) => (
-                                        <p key={uuid()}>
-                                            {game.user1 + " " + game.points1} - {game.points2 + " " + game.user2}
-                                        </p>))}
-                                    </tr>
-                                    : <></>
-								}
-                                {/* {clickedButton === rk.name
-                                ? <tr >{rk.games?.map((game: string) => (
-									<p key={uuid()}>
-                                        {game}
-                                    </p>))}
-                                </tr>
-                                : <></>
-								} */}
-						</StyledLeadP>
-						<StyledLeadP>{rk.wins}</StyledLeadP>
-						<StyledLeadP>{rk.lose}</StyledLeadP>
-						</tr>
-					</StyledLeadTile>))}
+                //                     ? <tr >{ tmppp?.map((game: GOT.Party) => (
+                    //                         <p key={uuid()}>
+                    //                             {game.user1 + " " + game.points1} - {game.points2 + " " + game.user2}
+                    //                         </p>))}
+                    //                     </tr>
+                    //                     : <></>
+                    // 				}
+                    //                 {/* {clickedButton === rk.name
+                    //                 ? <tr >{rk.games?.map((game: string) => (
+                        // 					<p key={uuid()}>
+                        //                         {game}
+                        //                     </p>))}
+                        //                 </tr>
+                        //                 : <></>
+                    // 				} */}
+                    // 		</StyledLeadP>
+                    // 		<StyledLeadP>{rk.wins}</StyledLeadP>
+                    // 		<StyledLeadP>{rk.lose}</StyledLeadP>
+                    // 		</tr>
+                    // 	</StyledLeadTile>))
+                    
+                }
 				</>
+				</StyledLeadTileRank>
+                    
+       
 			</StyledLead>
 			<Footer/>
 		</React.Fragment>
 	)
 }
-/*
-        <React.Fragment>
-            <BackgroundAnimate name="LeaderBoard"/>
-            <Header colorHome={Colors.MenuDisable} 
-                    colorGame={Colors.MenuDisable} 
-                    colorLeadBoard={Colors.MenuActive} 
-                    colorChat={Colors.MenuDisable}
-                    notify={notify}
-                    setNotify={setNotify}/>
-            <StyledLead>
-                <StyledTile>LeaderBoard</StyledTile>
-                <StyledSep/>
-                <StyledLeadTileRank color={Colors.Sep}>
-                <thead>
-                    <tr>
-                        <StyledLeadP>Rank</StyledLeadP>
-                        <StyledLeadP>Name</StyledLeadP>
-                        <StyledLeadP>Wins</StyledLeadP>
-                        <StyledLeadP>Loses</StyledLeadP>
-                    </tr>
-                </thead>
-                </StyledLeadTileRank>
-                {rank.map((rk: Ranks) => (
-                    <StyledLeadTile color={Colors.Rank} key={rk.id}>
-                        <thead>
-                            <tr>
-                                <StyledLeadP>{rk.rank}</StyledLeadP>
-                                <StyledLeadP>{rk.name}</StyledLeadP>
-                                <StyledLeadP>{rk.wins}</StyledLeadP>
-                                <StyledLeadP>{rk.lose}</StyledLeadP>
-                            </tr>
-                        </thead>
-                    </StyledLeadTile>
-                ))}
-            </StyledLead>
-            <Notification notify={notify} setNotify={setNotify}/>
-            <Footer/>
-        </React.Fragment>
-*/
 
 export default LeaderBoard;
-
