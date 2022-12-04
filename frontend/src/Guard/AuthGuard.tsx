2484c2de77207669933c8066dbcaa3f13df16ba3
waxcoin <waxcoin@hotmail.com>
2022-12-04 10:03:38 +0100

    Fix AuthGuard add useeffect tot async function for wait the answer and keep the user connect or not

--- frontend/src/Guard/AuthGuard.tsx
+++ frontend/src/Guard/AuthGuard.tsx
@@ -1,19 +1,29 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { accountService } from "../services/account.service";
 
 let access:boolean;
 
-const getAcces = async () => {
+const getAccess = async () => {
     let data = await accountService.isLogged();
     if (data === true)
         access = true;
     else
         access = false;
+    console.log(access)
 }
 
 const AuthGuard = ({children}:any) => {
-    (async () => await getAcces())()
-    if (access === false){
+    const [auth, setAuth] = useState<boolean>();
+    useEffect(() => {
+        (async () => {
+            await getAccess();
+            if (!access){
+                setAuth(access);
+            }
+        })();
+    },[])
+    if (auth === false){
         return <Navigate to="/"/>
     }
     return children;
