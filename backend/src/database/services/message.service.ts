6875e001c06e94d6819ad7edb21c8306548d2c15
valktaelen <alix.artiges@hotmail.fr>
2022-12-12 22:48:42 +0100

    fix error privmsg

--- backend/src/database/services/message.service.ts
+++ backend/src/database/services/message.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { GOT } from "shared/types";
 import { GeneralGateway } from "src/gateway/general.gateway";
-import { DeleteResult, Repository } from "typeorm";
+import { DeleteResult, IsNull, Repository } from "typeorm";
 import { Channel } from "../entities/channel.entity";
 import { Message } from "../entities/message.entity";
 import { User } from "../entities/user.entity";
@@ -40,8 +40,8 @@ export class MessageService {
 		return await this.messageRepository.find({
 			select: ["userFrom", "userTo"],
 			where: [
-				{userIdFrom: user.id, channelIdTo: undefined},
-				{userIdTo: user.id, channelIdTo: undefined}
+				{userIdFrom: user.id, channelIdTo: IsNull()},
+				{userIdTo: user.id, channelIdTo: IsNull()}
 			],
 			relations: ["userFrom", "userTo"]
 		});
